bc9f6b329aca95a0c6b84d82825172188a090b65
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-03-27 09:26:06 +0700

    Update hello_write.c

--- hello_write.c
+++ hello_write.c
@@ -1,6 +1,15 @@
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+#define STDOUT 1
 #include <unistd.h>
 
-int main() {
-    write(1, "Hello from write!\n", 16);
-    return 0;
+int main(void) {
+    ssize_t sz = write(STDOUT, "hello from write \n", 18);
+    return (sz == -1) ? EXIT_FAILURE : EXIT_SUCCESS;
 }
+
+
+
+
+
+
