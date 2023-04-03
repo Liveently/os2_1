bc29795f78c8daf01d7a78a2cbce49225084c92e
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-04-03 08:01:59 +0700

    Update hello_syscall.c

--- hello_syscall.c
+++ hello_syscall.c
@@ -1,9 +1,8 @@
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE 1
-#define STDOUT 1
-
 #include <sys/syscall.h>
 #include <unistd.h>
+
+#define STDOUT 1
+#define ERROR -1
 
 static int my_write(const int fd, const void* buf, const size_t count) {
     return syscall(SYS_write, fd, buf, count);.
@@ -11,7 +10,7 @@ static int my_write(const int fd, const void* buf, const size_t count) {
 
 int main() {
     int sz=my_write(STDOUT, "hello from my_write\n", 20);
-    return (sz == -1) ? EXIT_FAILURE : EXIT_SUCCESS;
+    return (sz == ERROR) ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
 
